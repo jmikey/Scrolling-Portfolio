97393dfae55f1b4af525701a31567f70191e142e
jMikeB <info@michaelbowlin.com>
2013-10-09 23:53:04 -0600

    Added side scroll to "Skills" page. Changed CSS/graphics. Added sub-navigation CSS3 animation.

--- js/custom.js
+++ js/custom.js
@@ -29,4 +29,44 @@ $(document).ready(function() {
     
   });
   
+
+
+        /* SIDE SCROLL */
+        $(function() {
+            $('#options ul a').bind('click',function(e){
+                //var $anchor = $(this);
+                /*
+                if you want to use one of the easing effects:
+                $('html, body').stop().animate({
+                    scrollLeft: $($anchor.attr('href')).offset().left
+                }, 1500,'easeInOutExpo');*/
+                        var $anchor = $(this);
+                        /*
+                        if you want to use one of the easing effects:
+                        $('html, body').stop().animate({
+                            scrollLeft: $($anchor.attr('href')).offset().left
+                        }, 1500,'easeInOutExpo');
+                         */
+                         if(($(this).attr('href')) === '#section1'){
+                          $('#skills .port2').stop().animate({
+                            "left":"0px"
+                          }, 'slow');
+                        } else if (($(this).attr('href')) === '#section2'){
+                          $('#skills .port2').stop().animate({
+                            "left":"-1500px"
+                          }, 'slow');
+                        } else if (($(this).attr('href')) === '#section3'){
+                          $('#skills .port2').stop().animate({
+                            "left":"-3000px"
+                          }, 'slow');
+                        }
+
+                        e.preventDefault();
+
+                console.log( $(this).attr('href') ) ;
+                //$('#skills .port').animate({"left":"300px"},'slow')
+
+            });
+        });
+
 });
